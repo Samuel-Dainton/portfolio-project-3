f058a3b7aed2e638460430bb68a268188c7c84c7
Samuel Dainton <dexterdainton@gmail.com>
2022-04-23 09:40:28 +0000

    Feat: import and test  termcolor and pyfiglet for customizing  output text.

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pyfiglet import Figlet
+figlet = Figlet(font="slant")
+import sys
+from termcolor import colored, cprint
+
+print(figlet.renderText("Hello World!"))
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'magenta', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
