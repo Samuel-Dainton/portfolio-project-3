cc2afa5edc59512ba77155f75ef23a47c44ff3d0
Samuel Dainton <dexterdainton@gmail.com>
2022-04-23 09:59:38 +0000

    Feat: Hangman image using ascii art.

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ figlet = Figlet(font="slant")
 import sys
 from termcolor import colored, cprint
 
-print(figlet.renderText("Hello World!"))
+# print(figlet.renderText("Hello World!"))
 
-text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
-print(text)
-cprint('Hello, World!', 'green', 'on_red')
+# cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
 
-print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
-print_red_on_cyan('Hello, World!')
-print_red_on_cyan('Hello, Universe!')
-
-for i in range(10):
-    cprint(i, 'magenta', end=' ')
-
-cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
+# print("""
+#         ___________
+#         | /       |
+#         ||        O
+#         ||       /|\\
+#         ||________|____
+#         ||       / \\   /|
+#         | \\           /
+#         L____________/
+#         ||          ||
+#         """)
